61b6b62ca3deaffac54feebcfbca3bf418e87da7
Matthew Liu <mcliu@andrew.cmu.edu>
2019-11-08 01:51:54 -0500

    Added scrolling list of cards for feed/profile

---
+++ tripbook-app/View/ProfileView.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileView.swift
+//  tripbook-app
+//
+//  Created by Matt Liu on 11/8/19.
+//  Copyright © 2019 67442. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+      //NavigationView {
+      //  List {
+      //    CardView()
+      //  }.navigationBarTitle(Text("My Trips"))
+      //}
+        Text("Hello, World!")
+      
+      //CardView(image: "Landscape1", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
