9789210fd137a7a1e52d12607c0c823dba9bfecf
Matthew Liu <mcliu@andrew.cmu.edu>
2019-12-06 07:27:53 -0500

    bugfix

--- tripbook-app/ViewModel/ProfileViewModel.swift
+++ tripbook-app/ViewModel/ProfileViewModel.swift
@@ -33,7 +33,7 @@ class ProfileViewModel : ObservableObject, Identifiable {
                   if user.data()!["fname"] as! String == "Jon" {
                     let f = user.data()!["fname"] as! String
                     let l = user.data()!["lname"] as! String
-                    let temp = Post(id: post.documentID,  post_annotation: post.data()["post_annotation"] as! String, post_images: ["Landscape" + String(self.count)], date: post.data()["date"] as! Timestamp, username: f + " " + l, profile_pic: "Profile_pic2")
+                    let temp = Post(id: post.documentID,  post_annotation: post.data()["post_annotation"] as! String, post_images: ["Landscape" + String(self.count)], date: post.data()["date"] as! Timestamp, username: f + " " + l, profile_pic: "Profile_pic3")
                     self.posts.append(temp)
                     self.count += 1
                   }
